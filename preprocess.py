02e65d628f6d2d84f3a97eb055a8af08ee3252cc
Prateek Joshi <pjoshi22900@gmail.com>
2021-09-24 17:47:04 +0530

    Add class segregator function

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,37 @@
 import glob
 import os
 import cv2 as cv
+import numpy as np
 
 DATA_DIR = os.path.join('data','NISTSpecialDatabase4GrayScaleImagesofFIGS','NISTSpecialDatabase4GrayScaleImagesofFIGS','sd04','png_txt')
+BASE_DIR = os.path.join('data','fingerprints')
+
+def get_class_name(file, folder):
+    filepath = os.path.join(folder, file+'.txt')
+    with open(filepath, 'r') as f:
+        class_name_line = f.readlines()[1].rstrip()
+        class_name = class_name_line[-1]
+    
+    return class_name
+
+def segregate_into_classes(png_files):
+    filenames = []
+    for file in png_files:
+        filename = os.path.split(file)[-1]
+        folder = os.path.split(file)[-2]
+        filenames.append(filename.split('.')[0])
+
+    for file in filenames:
+        class_name = get_class_name(file, folder)
+
+        # Create class directory if not exists
+        if not os.path.exists(os.path.join(BASE_DIR,class_name)):
+            os.mkdir(os.path.join(BASE_DIR,class_name))
+
+        os.rename(os.path.join(folder,file+'.png'), os.path.join(BASE_DIR,class_name,file+'.png'))
+
 
 # create base directory if not exists
-BASE_DIR = os.path.join('data','fingerprints')
 if not os.path.exists(BASE_DIR):
     os.mkdir(BASE_DIR)
 
@@ -22,7 +48,4 @@ for folder in folders_list:
     png_files = glob.glob(os.path.join(folder_path,'*.png'))
     txt_files = glob.glob(os.path.join(folder_path,'*.txt'))
 
-    # TODO: function to preprocess image and seperate into folders based on class
-    segregate_into_classes(png_files, txt_files)
-
-    break
+    segregate_into_classes(png_files)
