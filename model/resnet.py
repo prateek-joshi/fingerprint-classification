6dc19c923cd67bad4eed26fca69f12345b2e917a
Prateek Joshi <pjoshi22900@gmail.com>
2021-10-02 15:08:27 +0530

    Update model architecture

--- model/resnet.py
+++ model/resnet.py
@@ -1,13 +1,13 @@
 from tensorflow import keras
 from model.utils import IdentityBlock
 
-class Resnet50(keras.models.Model):
+class Resnet18(keras.models.Model):
   def __init__(self, num_classes, **kwargs):
-    super(Resnet50, self).__init__(**kwargs)
+    super(Resnet18, self).__init__(**kwargs)
 
-    self.conv = keras.layers.Conv2D(64, (7,7), padding='same')
+    self.conv = keras.layers.Conv2D(64, (7,7), padding='same', stride=2)
     self.btch = keras.layers.BatchNormalization()
-    self.maxpool = keras.layers.MaxPool2D((3,3))
+    self.maxpool = keras.layers.MaxPool2D((3,3), stride=2)
 
     self.conv_block_1 = IdentityBlock(64, 3, name='idblock-1', enhanced=True)
     self.id_block_1 = [IdentityBlock(64, 3, name='idblock-2'),IdentityBlock(64, 3, name='idblock-3')]
@@ -18,20 +18,8 @@ class Resnet50(keras.models.Model):
         IdentityBlock(64, 3, name='idblock-7')
     ]
 
-    self.conv_block_3 = IdentityBlock(64, 3, name='idblock-8', enhanced=True)
-    self.id_block_3 = [
-        IdentityBlock(64, 3, name='idblock-9'),IdentityBlock(64, 3, name='idblock-10'),
-        IdentityBlock(64, 3, name='idblock-11'),IdentityBlock(64, 3, name='idblock-12'),
-        IdentityBlock(64, 3, name='idblock-13')
-    ]
-
-    self.conv_block_4 = IdentityBlock(64, 3, name='idblock-14', enhanced=True)
-    self.id_block_4 = [
-        IdentityBlock(64, 3, name='idblock-15'),
-        IdentityBlock(64, 3, name='idblock-16')
-    ]
-
     self.gavg_pool = keras.layers.GlobalAveragePooling2D()
+    self.fc = keras.layers.Dense(units=128, activation='relu')
     self.classifier = keras.layers.Dense(units=num_classes, activation='softmax')
 
   def call(self, inputs):
@@ -46,14 +34,7 @@ class Resnet50(keras.models.Model):
     x = self.conv_block_2(x)
     for id_block in self.id_block_2:
         x = id_block(x)
-
-    x = self.conv_block_3(x)
-    for id_block in self.id_block_3:
-        x = id_block(x)
-
-    x = self.conv_block_4(x)
-    for id_block in self.id_block_4:
-        x = id_block(x)
     
     x = self.gavg_pool(x)
+    x = self.fc(x)
     return self.classifier(x)
