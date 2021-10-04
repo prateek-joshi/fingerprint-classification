d0ce0054e159a7b4db30ae79bfb3759407c21736
Prateek Joshi <pjoshi22900@gmail.com>
2021-10-04 22:23:05 +0530

    Update model architecture

--- model/resnet.py
+++ model/resnet.py
@@ -5,17 +5,17 @@ class Resnet18(keras.models.Model):
   def __init__(self, num_classes, **kwargs):
     super(Resnet18, self).__init__(**kwargs)
 
-    self.conv = keras.layers.Conv2D(64, (7,7), padding='same', strides=(2,2))
-    self.btch = keras.layers.BatchNormalization()
+    self.conv = keras.layers.Conv2D(32, (7,7), padding='same', strides=(2,2))
+    # self.btch = keras.layers.BatchNormalization()
     self.maxpool = keras.layers.MaxPool2D((3,3), strides=(2,2))
 
-    self.conv_block_1 = IdentityBlock(64, 3, name='idblock-1', enhanced=True)
-    self.id_block_1 = [IdentityBlock(64, 3, name='idblock-2'),IdentityBlock(64, 3, name='idblock-3')]
+    # self.conv_block_1 = IdentityBlock(32, 3, name='idblock-1', enhanced=True)
+    self.id_block_1 = [IdentityBlock(32, 3, name='idblock-2'),IdentityBlock(32, 3, name='idblock-3')]
 
-    self.conv_block_2 = IdentityBlock(64, 3, name='idblock-4', enhanced=True)
+    self.conv_block_2 = IdentityBlock(32, 3, name='idblock-4', enhanced=True)
     self.id_block_2 = [
-        IdentityBlock(64, 3, name='idblock-5'),IdentityBlock(64, 3, name='idblock-6'),
-        IdentityBlock(64, 3, name='idblock-7')
+        # IdentityBlock(32, 3, name='idblock-5'),IdentityBlock(32, 3, name='idblock-6'),
+        IdentityBlock(32, 3, name='idblock-7')
     ]
 
     self.gavg_pool = keras.layers.GlobalAveragePooling2D()
@@ -24,10 +24,10 @@ class Resnet18(keras.models.Model):
 
   def call(self, inputs):
     x = self.conv(inputs)
-    x = self.btch(x)
+    # x = self.btch(x)
     x = self.maxpool(x)
 
-    x = self.conv_block_1(x)
+    # x = self.conv_block_1(x)
     for id_block in self.id_block_1:
         x = id_block(x)
 
